3369f9db130bb2b083e5a91dd26021f1fb0ebb98
SzNeo8083 <128645227+SzNeo8083@users.noreply.github.com>
2025-06-24 19:40:06 +0200

    Update ThemeManager.lua

--- lib/ThemeManager.lua
+++ lib/ThemeManager.lua
@@ -50,7 +50,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'neohack'
+		local theme = 'Default'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
